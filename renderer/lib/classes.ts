2cc80e79ca8197048a668865b5b0c0ae45c6f438
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2024-12-22 17:05:24 +0100

    refactor: :recycle: improve classes and roles consts

--- renderer/lib/classes.ts
+++ renderer/lib/classes.ts
@@ -1,33 +1,22 @@
-export const CLASSES = [
-  "Demon Hunter",
-  "Rogue",
-  "Mage",
-  "Warrior",
-  "Druid",
-  "Paladin",
-  "Death Knight",
-  "Hunter",
-  "Shaman",
-  "Warlock",
-  "Priest",
-  "Monk",
-  "Evoker",
-].sort();
+import { wowClassSchema, wowRolesSchema } from "./schemas";
+import { WowClass } from "./types";
 
-export const classColors = new Map<string, string>([
-  ["Demon Hunter", "#a330c9"],
-  ["Rogue", "#fff468"],
-  ["Mage", "#3fc7eb"],
-  ["Warrior", "#c69b6d"],
-  ["Druid", "#ff7c0a"],
-  ["Paladin", "#f48cba"],
-  ["Death Knight", "#c41e3a"],
-  ["Hunter", "#aad372"],
-  ["Shaman", "#0070dd"],
-  ["Warlock", "#8788ee"],
-  ["Priest", "#ffffff"],
-  ["Monk", "#00ff98"],
-  ["Evoker", "#33937f"],
+export const CLASSES = wowClassSchema.options;
+
+export const ROLES = wowRolesSchema.options;
+
+export const classColors = new Map<WowClass, string>([
+    ["Demon Hunter", "#a330c9"],
+    ["Rogue", "#fff468"],
+    ["Mage", "#3fc7eb"],
+    ["Warrior", "#c69b6d"],
+    ["Druid", "#ff7c0a"],
+    ["Paladin", "#f48cba"],
+    ["Death Knight", "#c41e3a"],
+    ["Hunter", "#aad372"],
+    ["Shaman", "#0070dd"],
+    ["Warlock", "#8788ee"],
+    ["Priest", "#ffffff"],
+    ["Monk", "#00ff98"],
+    ["Evoker", "#33937f"],
 ]);
-
-export const ROLES = ["Tank", "Healer", "DPS"];
