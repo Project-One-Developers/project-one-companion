faadf009d49e463cc13869b1579769cd4bd105c3
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2024-12-22 15:57:21 +0100

    feat(droptimizer): :sparkles: add toast message for droptimizer's player not found

--- renderer/components/droptimizer-form.tsx
+++ renderer/components/droptimizer-form.tsx
@@ -1,127 +1,118 @@
+import { DroptimizerItem } from "@/lib/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Button } from "./ui/button";
-import {
-  FormField,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormMessage,
-  Form,
-} from "./ui/form";
-import { Input } from "./ui/input";
 import { useToast } from "./hooks/use-toast";
-import { DroptimizerItem } from "@/lib/types";
+import { Button } from "./ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
 
 const newDroptimizerSchema = z.object({
-  url: z.string().min(1),
+    url: z.string().min(1),
 });
 
 export default function NewDroptimizerForm() {
-  const form = useForm<z.infer<typeof newDroptimizerSchema>>({
-    resolver: zodResolver(newDroptimizerSchema),
-    defaultValues: {
-      url: "",
-    },
-  });
+    const form = useForm<z.infer<typeof newDroptimizerSchema>>({
+        resolver: zodResolver(newDroptimizerSchema),
+        defaultValues: {
+            url: "",
+        },
+    });
 
-  const { toast } = useToast();
+    const { toast } = useToast();
 
-  function persistPlayerUpgrade(data: {
-    playerName: string;
-    fightStyle: string;
-    targets: number;
-    time: number;
-    difficulty: string;
-    droptimizerUrl: string;
-    upgrade: DroptimizerItem[];
-  }) {
-    const players = JSON.parse(window.localStorage.getItem("players")) || [];
-    const playerIndex = players.findIndex((p) => p.character === data.playerName);
+    function persistPlayerUpgrade(data: {
+        playerName: string;
+        fightStyle: string;
+        targets: number;
+        time: number;
+        difficulty: string;
+        droptimizerUrl: string;
+        upgrade: DroptimizerItem[];
+    }) {
+        const players = JSON.parse(window.localStorage.getItem("players")) || [];
+        const playerIndex = players.findIndex((p) => p.character === data.playerName);
 
-    if (playerIndex === -1) {
-        console.log("Player not found");
-        return;
+        if (playerIndex === -1) {
+            toast({
+                title: "Player non esistente",
+                description: `Il pg ${data.playerName} non esiste.`,
+            });
+            return;
+        }
+
+        players[playerIndex] = {
+            ...players[playerIndex],
+            fightStyle: data.fightStyle,
+            targets: data.targets,
+            time: data.time,
+            difficulty: data.difficulty,
+            upgrades: data.upgrade,
+        };
+
+        window.localStorage.setItem("players", JSON.stringify(players));
+
+        toast({
+            title: "Aggiunta droptimizer",
+            description: `Il droptimizer per il pg ${data.playerName} è stato aggiunto con successo.`,
+        });
     }
 
-    players[playerIndex] = {
-        ...players[playerIndex],
-        fightStyle: data.fightStyle,
-        targets: data.targets,
-        time: data.time,
-        difficulty: data.difficulty,
-        upgrades: data.upgrade,
-    };
+    async function onSubmit(values: z.infer<typeof newDroptimizerSchema>) {
+        const response = await fetch(`${values.url}/data.csv`);
+        const data = await response.text();
+        let csvData = data.split("\n").map((row) => ({ name: row.split(",")[0], dmg: row.split(",")[1] }));
+        csvData = csvData.slice(1);
+        const charName = csvData[0].name;
+        const charBaseDmg = csvData[0].dmg;
 
-    window.localStorage.setItem("players", JSON.stringify(players));
+        const parsedData = csvData
+            .slice(1)
+            .map((d) => ({
+                name: d.name.split("/")[3],
+                dmg: Math.round(Number(d.dmg) - Number(charBaseDmg)),
+            }))
+            .filter((d) => d.dmg > 0);
 
-    toast({
-      title: "Aggiunta droptimizer",
-      description: `Il droptimizer per il pg ${data.playerName} è stato aggiunto con successo.`,
-    });
-  }
+        const response2 = await fetch(`${values.url}/data.json`);
+        const data2 = await response2.json();
+        const fightStyle = data2.sim.options.fight_style;
+        const targets = data2.sim.options.desired_targets;
+        const time = data2.sim.options.max_time;
+        const difficulty = data2.simbot.title.split("•")[2].replaceAll(" ", "");
 
-  async function onSubmit(values: z.infer<typeof newDroptimizerSchema>) {
-    const response = await fetch(`${values.url}/data.csv`);
-    const data = await response.text();
-    let csvData = data
-      .split("\n")
-      .map((row) => ({ name: row.split(",")[0], dmg: row.split(",")[1] }));
-    csvData = csvData.slice(1);
-    const charName = csvData[0].name;
-    const charBaseDmg = csvData[0].dmg;
+        persistPlayerUpgrade({
+            playerName: charName,
+            fightStyle,
+            targets,
+            time,
+            droptimizerUrl: values.url,
+            difficulty,
+            upgrade: parsedData,
+        });
+    }
 
-    const parsedData = csvData
-      .slice(1)
-      .map((d) => ({
-        name: d.name.split("/")[3],
-        dmg: Math.round(Number(d.dmg) - Number(charBaseDmg)),
-      }))
-      .filter((d) => d.dmg > 0);
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 min-w-[600px]">
+                <FormField
+                    control={form.control}
+                    name="url"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Droptimizer URL</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-    const response2 = await fetch(`${values.url}/data.json`);
-    const data2 = await response2.json();
-    const fightStyle = data2.sim.options.fight_style;
-    const targets = data2.sim.options.desired_targets;
-    const time = data2.sim.options.max_time;
-    const difficulty = data2.simbot.title.split("•")[2].replaceAll(" ", "");
-
-    persistPlayerUpgrade({
-      playerName: charName,
-      fightStyle,
-      targets,
-      time,
-      droptimizerUrl: values.url,
-      difficulty,
-      upgrade: parsedData,
-    });
-  }
-
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-4 min-w-[600px]"
-      >
-        <FormField
-          control={form.control}
-          name="url"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Droptimizer URL</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <Button type="submit" className="disabled:bg-gray-500">
-          Submit
-        </Button>
-      </form>
-    </Form>
-  );
+                <Button type="submit" className="disabled:bg-gray-500">
+                    Submit
+                </Button>
+            </form>
+        </Form>
+    );
 }
