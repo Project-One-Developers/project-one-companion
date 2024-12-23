2bd2ec83116d7b566dea06d486f380f4c3bb5bb3
Alessandro Neri <alessandro.neri@maggioli.it>
2024-12-23 10:27:51 +0100

    feat: aggiunta raid session schema

--- renderer/lib/storage/storage.schema.ts
+++ renderer/lib/storage/storage.schema.ts
@@ -1,4 +1,4 @@
-import { boolean, integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, text, timestamp, unique, varchar } from "drizzle-orm/pg-core";
 
 export const player = pgTable("players", {
   id: varchar('id').primaryKey(),
@@ -17,12 +17,26 @@ export const droptimizer = pgTable('droptimizers', {
   id: varchar('id').primaryKey(),
   url: text('url').notNull(),
   resultRaw: text('result_raw').notNull(),
-  droptimizerDate: timestamp('droptimizer_date').notNull(),
+  date: timestamp('date').notNull(),
   fightstyle: varchar('fightstyle', { length: 50 }).notNull(),
   duration: integer('duration').notNull(),
   raidDifficulty: varchar('raid_difficulty', { length: 20 }).notNull(),
   // charId: varchar("char_id").references(() => char.id),
 });
+
+export const raidSession = pgTable('raid_sessions', {
+  id: varchar('id').primaryKey(),
+  started_at: timestamp('date').notNull(),
+  completed_at: timestamp('date'),
+});
+
+export const raidSessionRoster = pgTable('raid_sessions_roster', {
+  id: varchar('id').primaryKey(),
+  raidSessionId: varchar('raid_session_id').references(() => raidSession.id),
+  charId: varchar('char_id').references(() => char.id),
+}, (t) => [{
+  unq: unique('raid_partecipation').on(t.raidSessionId, t.charId) // un char può partecipare ad una sessione alla volta
+}]);
 
 export const boss = pgTable('boss', {
   id: varchar('id').primaryKey(),
@@ -41,7 +55,8 @@ export const loot = pgTable('loots', {
   dropDate: timestamp('drop_date').notNull(),
   thirdStat: varchar('third_stat', { length: 255 }),
   socket: boolean('socket').notNull().default(false),
-  //eligibility: text('eligibility').array(), // array of IDs referencing RaidSession.Chars
+  // eligibility: text('eligibility').array(), // array of IDs referencing RaidSession.Chars
+  raidSessionId: varchar('raid_session_id').references(() => raidSession.id),
   itemId: varchar('item_id').references(() => item.id),
   bossId: varchar('boss_id').references(() => boss.id), // ridondato per comodità ma ricavabile da item.boss.id
 });
@@ -49,7 +64,7 @@ export const loot = pgTable('loots', {
 export const assignment = pgTable('assignments', {
   id: varchar('id').primaryKey(),
   charId: varchar("char_id").references(() => char.id),
-  lootId: varchar('loot_id').references(() => loot.id)
+  lootId: varchar('loot_id').references(() => loot.id).unique('loot_assignment'), // un loot può essere assegnato una sola volta
 });
 
 export const bisList = pgTable('bis_list', {
