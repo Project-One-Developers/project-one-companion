f9163f5c834cebc0097fb3e60ec6ccac9134a9e2
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2024-12-26 20:07:03 +0100

    perf(storage): :zap: upsert in single query, add util function to manage conflicts

--- src/main/lib/storage/boss/boss.storage.ts
+++ src/main/lib/storage/boss/boss.storage.ts
@@ -1,19 +1,15 @@
-import { eq } from 'drizzle-orm'
-import { match } from 'ts-pattern'
 import { Boss } from '../../../../../shared/types'
 import { db } from '../storage.config'
 import { bossTable } from '../storage.schema'
+import { buildConflictUpdateColumns } from '../storage.utils'
 
 export const upsertBosses = async (bosses: Boss[]): Promise<void> => {
-    const bossesInDb = await db.query.bossTable.findMany()
-
-    const upserts = bosses.map((boss) => {
-        const isBossInDb = bossesInDb.find((b) => b.id === boss.id)
-
-        return match(isBossInDb)
-            .with(undefined, () => db.insert(bossTable).values(boss))
-            .otherwise(() => db.update(bossTable).set(boss).where(eq(bossTable.id, boss.id)))
-    })
-
-    await Promise.all(upserts)
+    await db
+        .insert(bossTable)
+        .values(bosses)
+        .onConflictDoUpdate({
+            target: bossTable.id,
+            set: buildConflictUpdateColumns(bossTable, ['name', 'raid', 'order']) // TODO: generate columns dynamically
+        })
+        .execute()
 }
--- src/main/lib/storage/storage.utils.ts
+++ src/main/lib/storage/storage.utils.ts
@@ -1,3 +1,6 @@
+import { getTableColumns, sql, SQL } from 'drizzle-orm'
+import { PgTable } from 'drizzle-orm/pg-core'
+import { SQLiteTable } from 'drizzle-orm/sqlite-core'
 import { isPresent } from '../utils'
 
 // export const parseAndValidate = <Schema extends ZodTypeAny>(
@@ -20,3 +23,23 @@ import { isPresent } from '../utils'
 
 export const takeFirstResult = <T>(results: T[]): T | null =>
     results.length > 0 && isPresent(results[0]) ? results[0] : null
+
+export const buildConflictUpdateColumns = <
+    T extends PgTable | SQLiteTable,
+    Q extends keyof T['_']['columns']
+>(
+    table: T,
+    columns: Q[]
+): Record<Q, SQL<unknown>> => {
+    const cls = getTableColumns(table)
+
+    return columns.reduce(
+        (acc, column) => {
+            const colName = cls[column].name
+            acc[column] = sql.raw(`excluded.${colName}`)
+
+            return acc
+        },
+        {} as Record<Q, SQL>
+    )
+}
