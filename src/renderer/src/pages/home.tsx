ddf926a77efcd4e0695e4787e69a20c241fa9008
Lorenzo Galassi <lorenzo.galassi@quinck.io>
2024-12-27 10:41:29 +0100

    refactor: :bento: moved logo in a proper folder

--- src/renderer/src/pages/home.tsx
+++ src/renderer/src/pages/home.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
-
 export default function HomePage(): JSX.Element {
     return (
-        <React.Fragment>
+        <>
             <div className="flex flex-col items-center w-full justify-center mt-10 mb-10">
                 <div>
                     <img
                         className="ml-auto mr-auto"
-                        src="/images/logo.png"
+                        src="./assets/logo.png"
                         alt="Logo image"
                         width={256}
                         height={256}
                     />
                 </div>
             </div>
-        </React.Fragment>
+        </>
     )
 }
