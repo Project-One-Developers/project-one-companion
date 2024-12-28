18e028d71c3a117eb994e732e14d986f62dff01c
Lorenzo Galassi <lorenzo.galassi@quinck.io>
2024-12-28 13:05:32 +0100

    feat: :lipstick: added Geist font as default

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,11 @@ module.exports = {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out'
             }
+        },
+        fontFamily: {
+            sans: ['"Geist"', 'sans-serif'],
+            serif: ['"Geist"', 'serif'],
+            mono: ['"Geist"', 'monospace']
         }
     },
     plugins: [require('tailwindcss-animate')]
