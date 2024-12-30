02c2a82a5acb2f56a3a68bffe6697ba93be1a7f9
Alessandro Neri <alessandro.neri@maggioli.it>
2024-12-30 11:41:29 +0100

    chore: new script for prod db migration (inside .env.production)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,8 @@ if (!connectionUrl) {
     throw new Error('DATABASE_URL environment variable is not set')
 }
 
+console.log(`DB: configured ${connectionUrl}`)
+
 export default defineConfig({
     dialect: 'postgresql',
     schema: './src/main/lib/storage/storage.schema.ts',
