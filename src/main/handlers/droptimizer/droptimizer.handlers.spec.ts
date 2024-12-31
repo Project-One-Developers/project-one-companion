cb7aef00ff7eae30c258a29f05e833ffb8219efe
Alessandro Neri <alessandro.neri@maggioli.it>
2024-12-31 12:48:49 +0100

    feat(tests): simple droptimizer now materialized as testing data (bubbledan)

--- src/main/handlers/droptimizer/droptimizer.handlers.spec.ts
+++ src/main/handlers/droptimizer/droptimizer.handlers.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from '@jest/globals'
+import { db } from '@storage/storage.config'
 import { allHandlers } from '..'
-import dbMock from '../../setupTests'
 import { addDroptimizerHandler } from './droptimizer.handlers'
 
 describe('Droptimizer Handlers', () => {
@@ -12,19 +12,18 @@ describe('Droptimizer Handlers', () => {
 
     it('should throw an error when URL is not provided', async () => {
         await expect(allHandlers['add-droptimizer']('')).rejects.toThrow() // non mi interessa validare l'errore specifico
-        expect(dbMock.insert).not.toHaveBeenCalled()
+        expect(db.insert).not.toHaveBeenCalled()
     })
 
     it('should call addDroptimizerHandler with correct arguments and insert into database', async () => {
         const testUrl = 'https://www.raidbots.com/simbot/report/cUt45Z5FcaxztdQF9Girzx'
-        //const mockInsertResult = { id: '1', url: testUrl, date: new Date().toISOString() }
 
         // Mock the insert function to return our mock result
-        //dbMock.insert.mockResolvedValue([mockInsertResult])
+        //db.insert.mockResolvedValue([mockInsertResult])
 
         const result = await addDroptimizerHandler(testUrl)
 
-        expect(dbMock.transaction).toHaveBeenCalledTimes(1)
-        //expect(result).toEqual(mockInsertResult)
+        expect(db.transaction).toHaveBeenCalledTimes(1)
+        expect(result).toEqual({})
     })
 })
--- src/main/setupTests.ts
+++ src/main/setupTests.ts
@@ -1,8 +1,27 @@
-import { beforeEach, jest } from '@jest/globals'
-import { DeepMockProxy, mockDeep, mockReset } from 'jest-mock-extended'
+import { jest } from '@jest/globals'
+import fs from 'fs'
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended'
+import path from 'path'
 
 // Create a mock for the db
 const dbMock: DeepMockProxy<typeof import('./lib/storage/storage.config').db> = mockDeep()
+const fetchRaidbotsDataMock = jest.fn((url: string) => {
+    console.log(`Mocking return value for ${url}`)
+    const mockCsvData = fs.readFileSync(
+        path.resolve(__dirname, '../../resources/raidbots/testData/data.csv'),
+        'utf8'
+    )
+    const mockJsonData = JSON.parse(
+        fs.readFileSync(
+            path.resolve(__dirname, '../../resources/raidbots/testData/data.json'),
+            'utf8'
+        )
+    )
+    return Promise.resolve({
+        csvData: mockCsvData,
+        jsonData: mockJsonData
+    })
+})
 
 // Mock the entire module
 jest.mock('./lib/storage/storage.config', () => ({
@@ -10,8 +29,13 @@ jest.mock('./lib/storage/storage.config', () => ({
     db: dbMock
 }))
 
-beforeEach(() => {
-    mockReset(dbMock)
-})
+// Mock the fetchRaidbotsData function
+jest.mock('./handlers/droptimizer/droptimizer.utils', () => ({
+    __esModule: true,
+    fetchRaidbotsData: fetchRaidbotsDataMock
+}))
 
-export default dbMock
+// beforeEach(() => {
+//     mockReset(dbMock)
+//     mockReset(fetchRaidbotsDataMock)
+// })
