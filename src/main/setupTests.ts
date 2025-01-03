bdc24f1afe5c5f855393496c2eb072024b8169c2
Alessandro Neri <alessandro.neri@maggioli.it>
2025-01-03 17:45:45 +0100

    fix(test): mock db initialization

---
+++ src/main/setupTests.ts
@@ -0,0 +1,9 @@
+import { jest } from '@jest/globals'
+
+// Create a mock for the db
+
+// Mock the entire module
+jest.mock('./lib/storage/storage.config', () => ({
+    __esModule: true,
+    db: jest.fn()
+}))
