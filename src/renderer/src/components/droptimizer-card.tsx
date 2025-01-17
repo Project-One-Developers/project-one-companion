b7eeb4f2882c2eec224b3bd502123df9c3ce0c24
Alessandro Neri <alessandro.neri@maggioli.it>
2025-01-17 18:41:52 +0100

    refactor(droptimizer): :art: enchance component structure & implement delete action

--- src/renderer/src/components/droptimizer-card.tsx
+++ src/renderer/src/components/droptimizer-card.tsx
@@ -1,120 +1,162 @@
+import { deleteDroptimizer } from '@renderer/lib/tanstack-query/droptimizers'
 import { queryKeys } from '@renderer/lib/tanstack-query/keys'
 import { fetchRaidLootTable } from '@renderer/lib/tanstack-query/raid'
 import { formatUnixTimestampToRelativeDays, getDpsHumanReadable } from '@renderer/lib/utils'
-import { Boss, Droptimizer, Item } from '@shared/types/types'
-import { useQuery } from '@tanstack/react-query'
-import { LoaderCircle } from 'lucide-react'
+import { Boss, Droptimizer, DroptimizerUpgrade, Item, WowRaidDifficulty } from '@shared/types/types'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { LoaderCircle, X } from 'lucide-react'
 import { useState } from 'react'
 import DroptimizerDetailDialog from './droptimizer-detail-dialog'
+import { Button } from './ui/button'
 import { Dialog, DialogTrigger } from './ui/dialog'
 import { WowItemIcon } from './ui/wowitem-icon'
 import { WowSpecIcon } from './ui/wowspec-icon'
+
+const CharacterInfo = ({ charInfo }: { charInfo: Droptimizer['charInfo'] }) => (
+    <div className="flex items-center space-x-3">
+        <WowSpecIcon
+            specId={charInfo.specId}
+            className="object-cover object-top rounded-md full h-10 w-10 border border-background"
+        />
+        <h2 className="font-black">{charInfo.name}</h2>
+    </div>
+)
+
+const DroptimizerInfo = ({ dropt, bosses }: { dropt: Droptimizer; bosses: Boss[] }) => {
+    const [isOpen, setOpen] = useState(false)
+    return (
+        <div className="text-xs mt-3">
+            <p>
+                <strong>Raid Difficulty:</strong> {dropt.raidInfo.difficulty}
+            </p>
+            <p>
+                <strong>Fight Style:</strong> {dropt.simInfo.fightstyle} ({dropt.simInfo.nTargets}){' '}
+                {dropt.simInfo.duration} sec
+            </p>
+            <p title={new Date(dropt.simInfo.date * 1000).toLocaleString()}>
+                <strong>Date: </strong>
+                {formatUnixTimestampToRelativeDays(dropt.simInfo.date)}
+            </p>
+            <Dialog open={isOpen} onOpenChange={setOpen}>
+                <DialogTrigger asChild className="cursor-pointer q3 links">
+                    <p>
+                        <strong>Upgrades:</strong> {dropt.upgrades?.length}
+                    </p>
+                </DialogTrigger>
+                <DroptimizerDetailDialog droptimizer={dropt} bosses={bosses} />
+            </Dialog>
+        </div>
+    )
+}
+
+const UpgradeItem = ({
+    upgrade,
+    raidItems,
+    raidDifficulty
+}: {
+    upgrade: DroptimizerUpgrade
+    raidItems: Item[]
+    raidDifficulty: WowRaidDifficulty
+}) => {
+    const foundItem = raidItems.find((item) => item.id === upgrade.item.id)
+
+    return (
+        <div className="-mr-4 relative group">
+            {foundItem ? (
+                <WowItemIcon
+                    item={foundItem}
+                    iconOnly={true}
+                    raidDiff={raidDifficulty}
+                    catalystBanner={upgrade.catalyzedItem != null}
+                    className="mt-2"
+                    iconClassName="object-cover object-top rounded-full h-10 w-10 border border-background"
+                />
+            ) : (
+                <a
+                    href={`https://www.wowhead.com/item=${upgrade.item.id}`}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="q3 links"
+                >
+                    <p>{upgrade.item.id}</p>
+                </a>
+            )}
+            <p className="text-xs text-center">
+                <strong>{getDpsHumanReadable(upgrade.dps)}</strong>
+            </p>
+        </div>
+    )
+}
 
 type DroptimizerCardProps = {
     droptimizer: Droptimizer
 }
 
 export const DroptimizerCard = ({ droptimizer: dropt }: DroptimizerCardProps) => {
-    // droptimizer detail dialog
-    const [isOpen, setOpen] = useState(false)
-    const itemRes = useQuery({
+    const queryClient = useQueryClient()
+    const { data: bosses = [], isLoading } = useQuery({
         queryKey: [queryKeys.raidLootTable, dropt.raidInfo.id],
         queryFn: () => fetchRaidLootTable(dropt.raidInfo.id)
     })
-    const bosses: Boss[] = itemRes.data ?? []
-    const raidItems: Item[] = itemRes.data?.flatMap((boss) => boss.items) ?? []
-    const raidItemsIsLoading = itemRes.isLoading
-    return (
-        <>
-            {raidItemsIsLoading ? (
-                <div className="flex flex-col items-center w-full justify-center mt-10 mb-10">
-                    <LoaderCircle className="animate-spin text-5xl" />
-                </div>
-            ) : (
-                <Dialog open={isOpen} onOpenChange={setOpen}>
-                    <div className="flex flex-col justify-between p-6 bg-muted h-[230px] w-[310px] rounded-lg relative">
-                        {/* Character Info */}
-                        <div className="flex items-center space-x-3">
-                            <WowSpecIcon
-                                specId={dropt.charInfo.specId}
-                                className="object-cover object-top rounded-md full h-10 w-10 border border-background"
-                            />
-                            <h2 className="font-black">{dropt.charInfo.name}</h2>
-                        </div>
 
-                        {/* Droptimizer Info */}
-                        <div className="text-xs mt-3">
-                            <p>
-                                <strong>Raid Difficulty:</strong> {dropt.raidInfo.difficulty}
-                            </p>
-                            <p>
-                                <strong>Fight Style:</strong> {dropt.simInfo.fightstyle} (
-                                {dropt.simInfo.nTargets}) {dropt.simInfo.duration} sec
-                            </p>
-                            <p title={new Date(dropt.simInfo.date * 1000).toLocaleString()}>
-                                <strong>Date: </strong>
-                                {formatUnixTimestampToRelativeDays(dropt.simInfo.date)}
-                            </p>
-                            <DialogTrigger asChild className="cursor-pointer q3 links">
-                                <p>
-                                    <strong>Upgrades:</strong> {dropt.upgrades?.length}
-                                </p>
-                            </DialogTrigger>
-                        </div>
-                        {raidItemsIsLoading ? (
-                            <div className="flex flex-col items-center w-full justify-center mt-10 mb-10">
-                                <LoaderCircle className="animate-spin text-5xl" />
-                            </div>
-                        ) : (
-                            <div className="flex items-center gap-3 mt-1">
-                                {dropt.upgrades &&
-                                    dropt.upgrades
-                                        .sort((a, b) => b.dps - a.dps)
-                                        .slice(0, 7)
-                                        .map((upgrade) => {
-                                            const foundItem = raidItems.find(
-                                                (item) => item.id === upgrade.item.id
-                                            )
-                                            return (
-                                                <div
-                                                    className="-mr-4 relative group"
-                                                    key={upgrade.item.id}
-                                                >
-                                                    {foundItem ? (
-                                                        <WowItemIcon
-                                                            item={foundItem}
-                                                            iconOnly={true}
-                                                            raidDiff={dropt.raidInfo.difficulty}
-                                                            catalystBanner={
-                                                                upgrade.catalyzedItem != null
-                                                            }
-                                                            className="mt-2"
-                                                            iconClassName="object-cover object-top rounded-full h-10 w-10 border border-background"
-                                                        />
-                                                    ) : (
-                                                        <a
-                                                            href={`https://www.wowhead.com/item=${upgrade.item.id}`}
-                                                            rel="noreferrer"
-                                                            target="_blank"
-                                                            className="q3 links"
-                                                        >
-                                                            <p>{upgrade.item.id}</p>
-                                                        </a>
-                                                    )}
-                                                    <p className="text-xs text-center">
-                                                        <strong>
-                                                            {getDpsHumanReadable(upgrade.dps)}
-                                                        </strong>
-                                                    </p>
-                                                </div>
-                                            )
-                                        })}
-                            </div>
-                        )}
-                    </div>
-                    <DroptimizerDetailDialog droptimizer={dropt} bosses={bosses} />
-                </Dialog>
-            )}
-        </>
+    const deleteMutation = useMutation({
+        mutationFn: deleteDroptimizer,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [queryKeys.droptimizers] })
+        }
+    })
+
+    const handleDelete = (e: React.MouseEvent) => {
+        e.stopPropagation()
+        if (
+            window.confirm(`Are you sure you want to delete ${dropt.charInfo.name}'s droptimizer?`)
+        ) {
+            deleteMutation.mutate(dropt.url)
+        }
+    }
+
+    const raidItems = bosses.flatMap((boss) => boss.items)
+
+    if (isLoading) {
+        return (
+            <div className="flex flex-col items-center w-full justify-center mt-10 mb-10">
+                <LoaderCircle className="animate-spin text-5xl" />
+            </div>
+        )
+    }
+
+    return (
+        <div className="flex flex-col justify-between p-6 bg-muted h-[230px] w-[310px] rounded-lg relative">
+            {/* Top Right Menu */}
+            <Button
+                variant="ghost"
+                size="icon"
+                className="absolute top-3 right-2"
+                onClick={handleDelete}
+            >
+                <X className="h-4 w-4" />
+            </Button>
+
+            {/* Spec Icon + Name */}
+            <CharacterInfo charInfo={dropt.charInfo} />
+
+            {/* Sim summary + detail dialog*/}
+            <DroptimizerInfo dropt={dropt} bosses={bosses} />
+
+            {/* Upgrades */}
+            <div className="flex items-center gap-3 mt-1">
+                {dropt.upgrades
+                    ?.sort((a, b) => b.dps - a.dps)
+                    .slice(0, 7)
+                    .map((upgrade) => (
+                        <UpgradeItem
+                            key={upgrade.item.id}
+                            upgrade={upgrade}
+                            raidItems={raidItems}
+                            raidDifficulty={dropt.raidInfo.difficulty}
+                        />
+                    ))}
+            </div>
+        </div>
     )
 }
