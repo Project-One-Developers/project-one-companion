959421f4aa377460e7fb6d489bde495e29c469cd
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2025-02-08 20:20:56 +0100

    refactor: :recycle: add return type to getClassSpec function

--- shared/libs/spec-parser/spec-parser.ts
+++ shared/libs/spec-parser/spec-parser.ts
@@ -1,6 +1,7 @@
+import { WowClass, WowSpec } from '@shared/types/types'
 import { SPEC_ID_TO_CLASS_SPEC } from './spec-parser.schemas'
 
-export const getClassSpec = (specId: number) => {
+export const getClassSpec = (specId: number): { wowClass: WowClass; wowSpec: WowSpec } => {
     const mapping = SPEC_ID_TO_CLASS_SPEC[specId]
     if (!mapping) {
         throw new Error(`Invalid spec ID: ${specId}`)
