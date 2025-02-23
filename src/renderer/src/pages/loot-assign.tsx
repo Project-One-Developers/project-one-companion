d0054992dff84ebd87a2fda38346978035e262cd
Alessandro Neri <alessandro.neri@maggioli.it>
2025-02-23 21:58:37 +0100

    fix: loot assign improvements

--- src/renderer/src/pages/loot-assign.tsx
+++ src/renderer/src/pages/loot-assign.tsx
@@ -7,7 +7,7 @@ import { fetchRaidSessions } from '@renderer/lib/tanstack-query/raid'
 import { LootWithAssigned } from '@shared/types/types'
 import { useQuery } from '@tanstack/react-query'
 import { LoaderCircle } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export default function LootAssign() {
     const [selectedSessions, setSelectedSessions] = useState<Set<string>>(new Set())
@@ -22,7 +22,7 @@ export default function LootAssign() {
         queryKey: [queryKeys.lootsBySession, Array.from(selectedSessions)],
         queryFn: () => getLootsWithAssignedBySessions(Array.from(selectedSessions)),
         enabled: selectedSessions.size > 0,
-        refetchInterval: 10000 // Refetch every 10 seconds
+        refetchInterval: 10000 // Refetch every 10 seconds - TODO: update with server side events
     })
 
     // Update selectedLoot.assignedCharacterId if changed in backend
@@ -38,6 +38,16 @@ export default function LootAssign() {
         }
     }, [lootsQuery.data, selectedLoot])
 
+    const sortedSessions = useMemo(
+        () =>
+            raidSessionsQuery.data
+                ? [...raidSessionsQuery.data].sort((a, b) => b.raidDate - a.raidDate)
+                : [],
+        [raidSessionsQuery.data]
+    )
+
+    const loots = useMemo(() => lootsQuery.data ?? [], [lootsQuery.data])
+
     if (raidSessionsQuery.isLoading || lootsQuery.isLoading) {
         return (
             <div className="flex flex-col items-center w-full justify-center mt-10 mb-10">
@@ -45,11 +55,6 @@ export default function LootAssign() {
             </div>
         )
     }
-
-    const sessions = raidSessionsQuery.data
-        ? [...raidSessionsQuery.data].sort((a, b) => b.raidDate - a.raidDate)
-        : []
-    const loots = lootsQuery.data ?? []
 
     const toggleSession = (sessionId: string) => {
         const newSelectedSessions = new Set(selectedSessions)
@@ -65,7 +70,7 @@ export default function LootAssign() {
         <div className="w-dvw h-dvh flex flex-col gap-y-8 p-8 relative">
             {/* Page Header */}
             <div className=" bg-muted rounded-lg p-6 mb-2 shadow-lg flex items-center">
-                {sessions.map((session) => (
+                {sortedSessions.map((session) => (
                     <div key={session.id} onClick={() => toggleSession(session.id)}>
                         <SessionCard
                             session={session}
