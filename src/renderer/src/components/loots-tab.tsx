957574906fa2a206b33954f9e345023489c4200e
Alessandro Neri <alessandro.neri@maggioli.it>
2025-02-24 09:42:12 +0100

    fix: slot tabs are now icon

--- src/renderer/src/components/loots-tab.tsx
+++ src/renderer/src/components/loots-tab.tsx
@@ -1,6 +1,8 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs'
+import { itemSlotIcon } from '@renderer/lib/wow-icon'
 import { ITEM_SLOTS_KEY } from '@shared/consts/wow.consts'
 import { formatWowSlotKey } from '@shared/libs/items/item-slot-utils'
+import { wowItemSlotKeySchema } from '@shared/schemas/wow.schemas'
 import { LootWithAssigned } from '@shared/types/types'
 import LootItem from './loots-item'
 
@@ -45,16 +47,25 @@ const LootsTabs = ({ loots, selectedLoot, setSelectedLoot }: LootsTabsProps) => 
 
     return (
         <Tabs defaultValue={ITEM_SLOTS_KEY[0]}>
-            <TabsList className="flex flex-wrap space-x-2 overflow-x-auto pb-2">
-                {ITEM_SLOTS_KEY.map((slot) => (
-                    <TabsTrigger
-                        key={slot}
-                        value={slot}
-                        className="flex flex-col items-start gap-1 py-2 hover:bg-muted data-[state=active]:border-b-2 data-[state=active]:border-primary"
-                    >
-                        {formatWowSlotKey(slot)}
-                    </TabsTrigger>
-                ))}
+            <TabsList className="flex flex-wrap pb-2">
+                <div className="flex flex-wrap gap-2">
+                    {wowItemSlotKeySchema.options.map((slot) => (
+                        <TabsTrigger
+                            value={slot}
+                            key={slot}
+                            className="flex flex-col items-start gap-1 py-2 data-[state=active]:border-b-2 data-[state=active]:border-primary"
+                        >
+                            <div className={`cursor-pointer transition-transform hover:scale-125`}>
+                                <img
+                                    src={itemSlotIcon.get(slot)}
+                                    alt={slot}
+                                    className="w-7 h-7"
+                                    title={formatWowSlotKey(slot)}
+                                />
+                            </div>
+                        </TabsTrigger>
+                    ))}
+                </div>
             </TabsList>
             {ITEM_SLOTS_KEY.map((slot) => (
                 <TabsContent
