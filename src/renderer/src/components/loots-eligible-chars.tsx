0b6223903eb1c3e1c46c3923796c6e373027c81f
Alessandro Neri <alessandro.neri@maggioli.it>
2025-02-24 16:01:07 +0100

    refactor: more styled missing droptimizer

--- src/renderer/src/components/loots-eligible-chars.tsx
+++ src/renderer/src/components/loots-eligible-chars.tsx
@@ -11,13 +11,7 @@ import type {
     LootWithAssigned
 } from '@shared/types/types'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import {
-    HeartCrackIcon,
-    LoaderCircle,
-    MoreVertical,
-    ShieldAlertIcon,
-    TriangleAlertIcon
-} from 'lucide-react'
+import { HeartCrackIcon, LoaderCircle, MoreVertical, ShieldAlertIcon } from 'lucide-react'
 import { useMemo, useState, type JSX } from 'react'
 import { DroptimizerUpgradeForItemEquipped } from './droptimizer-upgrade-for-item'
 import { toast } from './hooks/use-toast'
@@ -298,15 +292,24 @@ export default function LootsEligibleChars({
                                             />
                                         ))}
                                         {charInfo.warnDroptimizer.type === 'not-imported' && (
-                                            <Tooltip>
-                                                <TooltipTrigger asChild>
-                                                    <TriangleAlertIcon className="text-yellow-400 cursor-pointer hover:text-yellow-300" />
-                                                </TooltipTrigger>
-                                                <TooltipContent className="bg-gray-800 text-gray-200 p-2 rounded-lg">
-                                                    Missing Droptimizer
-                                                    <TooltipArrow className="fill-gray-800" />
-                                                </TooltipContent>
-                                            </Tooltip>
+                                            <div className="flex flex-wrap gap-2">
+                                                <span className="px-2 py-1 text-xs font-bold bg-yellow-500/20 text-yellow-400 rounded-full flex items-center space-x-1 border border-yellow-400/50">
+                                                    {/* Warning Icon */}
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        className="h-3 w-3"
+                                                        viewBox="0 0 20 20"
+                                                        fill="currentColor"
+                                                    >
+                                                        <path
+                                                            fillRule="evenodd"
+                                                            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                                                            clipRule="evenodd"
+                                                        />
+                                                    </svg>
+                                                    <span>MISSING</span>
+                                                </span>
+                                            </div>
                                         )}
                                     </div>
                                 </TableCell>
