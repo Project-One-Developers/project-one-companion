9ef312d250b49dd9108d0dd44146d5cb77bd7eae
Alessandro Neri <alessandro.neri@maggioli.it>
2025-02-24 09:58:31 +0100

    fix: semplified loot tabs structure

--- src/renderer/src/components/loots-tab.tsx
+++ src/renderer/src/components/loots-tab.tsx
@@ -1,10 +1,11 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs'
 import { itemSlotIcon } from '@renderer/lib/wow-icon'
 import { ITEM_SLOTS_KEY } from '@shared/consts/wow.consts'
 import { formatWowSlotKey } from '@shared/libs/items/item-slot-utils'
 import { wowItemSlotKeySchema } from '@shared/schemas/wow.schemas'
-import { LootWithAssigned } from '@shared/types/types'
-import LootItem from './loots-item'
+import { LootWithAssigned, WowItemSlotKey } from '@shared/types/types'
+import { useState } from 'react'
+import { WowClassIcon } from './ui/wowclass-icon'
+import { WowGearIcon } from './ui/wowgear-icon'
 
 type LootsTabsProps = {
     loots: LootWithAssigned[]
@@ -12,72 +13,82 @@ type LootsTabsProps = {
     setSelectedLoot: (loot: LootWithAssigned) => void
 }
 
-const LootsTabs = ({ loots, selectedLoot, setSelectedLoot }: LootsTabsProps) => {
-    const renderLoots = (slot) => {
-        const filteredLoots = loots
-            .filter((loot) => loot.gearItem.item.slotKey === slot)
-            .sort((a, b) => {
-                if (a.gearItem.item.token !== b.gearItem.item.token) {
-                    return a.gearItem.item.token ? -1 : 1
-                }
-                if (a.gearItem.item.armorType !== b.gearItem.item.armorType) {
-                    if (a.gearItem.item.armorType === null) return 1
-                    if (b.gearItem.item.armorType === null) return -1
-                    return a.gearItem.item.armorType.localeCompare(b.gearItem.item.armorType)
-                }
-                if (a.gearItem.item.id !== b.gearItem.item.id) {
-                    return a.gearItem.item.id - b.gearItem.item.id
-                }
-                return a.id.localeCompare(b.id)
-            })
+const LootsList = ({ loots, selectedLoot, setSelectedLoot }: LootsTabsProps) => {
+    const [selectedSlot, setSelectedSlot] = useState<WowItemSlotKey>(ITEM_SLOTS_KEY[0])
 
-        if (filteredLoots.length === 0) {
-            return <p className="text-gray-400">No loot in this category</p>
-        }
-
-        return filteredLoots.map((loot, index) => (
-            <LootItem
-                key={index}
-                loot={loot}
-                isSelected={selectedLoot != null && selectedLoot.id === loot.id}
-                setSelectedLoot={setSelectedLoot}
-            />
-        ))
-    }
+    const filteredLoots = loots
+        .filter((loot) => loot.gearItem.item.slotKey === selectedSlot)
+        .sort((a, b) => {
+            if (a.gearItem.item.token !== b.gearItem.item.token)
+                return a.gearItem.item.token ? -1 : 1
+            if (a.gearItem.item.armorType !== b.gearItem.item.armorType) {
+                if (a.gearItem.item.armorType === null) return 1
+                if (b.gearItem.item.armorType === null) return -1
+                return a.gearItem.item.armorType.localeCompare(b.gearItem.item.armorType)
+            }
+            return a.gearItem.item.id - b.gearItem.item.id || a.id.localeCompare(b.id)
+        })
 
     return (
-        <Tabs defaultValue={ITEM_SLOTS_KEY[0]}>
-            <TabsList className="flex flex-wrap pb-2">
-                <div className="flex flex-wrap gap-2">
-                    {wowItemSlotKeySchema.options.map((slot) => (
-                        <TabsTrigger
-                            value={slot}
-                            key={slot}
-                            className="flex flex-col items-start gap-1 py-2 data-[state=active]:border-b-2 data-[state=active]:border-primary"
+        <div>
+            <div className="flex flex-wrap gap-2 pb-2">
+                {wowItemSlotKeySchema.options.map((slot) => (
+                    <button
+                        key={slot}
+                        onClick={() => setSelectedSlot(slot)}
+                        className={`flex flex-col items-center gap-1 p-1 border-b-2 transition-transform ${
+                            selectedSlot === slot ? 'border-primary' : 'border-transparent'
+                        }`}
+                    >
+                        <img
+                            src={itemSlotIcon.get(slot)}
+                            alt={slot}
+                            className="w-8 h-8 hover:scale-125 transition-transform cursor-pointer rounded-sm"
+                            title={formatWowSlotKey(slot)}
+                        />
+                    </button>
+                ))}
+            </div>
+
+            <div className="bg-muted p-4 rounded-lg shadow-md mt-2">
+                {filteredLoots.length === 0 ? (
+                    <p className="text-gray-400">No loot in this category</p>
+                ) : (
+                    filteredLoots.map((loot) => (
+                        <div
+                            key={loot.id}
+                            className={`flex flex-row justify-between border-b border-gray-700 py-2 cursor-pointer hover:bg-gray-700 p-2 rounded-md ${selectedLoot?.id === loot.id ? 'bg-gray-700' : ''}`}
+                            onClick={(e) => {
+                                e.preventDefault()
+                                setSelectedLoot(loot)
+                            }}
                         >
-                            <div className={`cursor-pointer transition-transform hover:scale-125`}>
-                                <img
-                                    src={itemSlotIcon.get(slot)}
-                                    alt={slot}
-                                    className="w-7 h-7"
-                                    title={formatWowSlotKey(slot)}
-                                />
-                            </div>
-                        </TabsTrigger>
-                    ))}
-                </div>
-            </TabsList>
-            {ITEM_SLOTS_KEY.map((slot) => (
-                <TabsContent
-                    key={slot}
-                    value={slot}
-                    className="bg-muted p-4 rounded-lg shadow-md mt-2"
-                >
-                    {renderLoots(slot)}
-                </TabsContent>
-            ))}
-        </Tabs>
+                            {/* Loot */}
+                            <WowGearIcon
+                                gearItem={loot.gearItem}
+                                showSlot={false}
+                                showTierBanner={true}
+                                showExtendedInfo={true}
+                                showArmorType={true}
+                            />
+
+                            {/* Assigned to Char */}
+                            {loot.assignedCharacter && (
+                                <div className="flex flex-row space-x-4 items-center ">
+                                    <p className="text-sm -mr-2">{loot.assignedCharacter.name}</p>
+                                    <WowClassIcon
+                                        wowClassName={loot.assignedCharacter.class}
+                                        charname={loot.assignedCharacter.name}
+                                        className="h-8 w-8 border-2 border-background rounded-lg"
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
     )
 }
 
-export default LootsTabs
+export default LootsList
